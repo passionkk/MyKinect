f444cacc2402fca111d5b1afdccd4f39d684aeb9
passionkk <767313547@qq.com>
2016-03-30 17:15:17 +0800

    手势移动

--- GestureDetect.cpp
+++ GestureDetect.cpp
@@ -6,7 +6,7 @@ CGestureDetect::CGestureDetect(void)
 {
 	m_gestureType = GESTURE_NONE;
 	m_skeletonPosIndex = NUI_SKELETON_POSITION_HAND_LEFT;
-	m_dequeSkeletonFrame.clear();
+	m_dequeSkeletonData.clear();
 }
 
 
@@ -21,20 +21,168 @@ void CGestureDetect::SetJudgePos( NUI_SKELETON_POSITION_INDEX posIndex )
 
 void CGestureDetect::SetSkeletonFrame( const NUI_SKELETON_FRAME& SkeletonFrame )
 {
-	m_dequeSkeletonFrame.push_back(SkeletonFrame);
-	while (m_dequeSkeletonFrame.size() > cSkeletonFrameMax)
+	//目前只计算一个人
+	for (int i = 0; i < NUI_SKELETON_COUNT; i++)
 	{
-		m_dequeSkeletonFrame.pop_front();
+		if (SkeletonFrame.SkeletonData[i].eTrackingState == NUI_SKELETON_TRACKED)
+		{
+			m_dequeSkeletonData.push_back(SkeletonFrame.SkeletonData[i]);
+			break;
+		}
+	}
+	
+	while (m_dequeSkeletonData.size() > cSkeletonFrameMax)
+	{
+		m_dequeSkeletonData.pop_front();
 	}
 }
 
-void CGestureDetect::GetCurrentGesture( GESTURE_TYPE& GestureType, bool bIsLeftHand )
+unsigned int CGestureDetect::GetCurrentGesture()
 {
-	GestureType = m_gestureType;
+	return m_gestureType;
+}
+
+bool CGestureDetect::IsInValidRange()
+{
+	bool bRet = false;
+
+	if (m_dequeSkeletonData.size() == cSkeletonFrameMax)
+	{
+		if (m_dequeSkeletonData.at(cSkeletonFrameMax-1).SkeletonPositions[m_skeletonPosIndex].y >
+			m_dequeSkeletonData.at(cSkeletonFrameMax-1).SkeletonPositions[NUI_SKELETON_POSITION_ELBOW_LEFT].y &&
+			m_dequeSkeletonData.at(cSkeletonFrameMax-1).SkeletonPositions[m_skeletonPosIndex].y <
+			m_dequeSkeletonData.at(cSkeletonFrameMax-1).SkeletonPositions[NUI_SKELETON_POSITION_HEAD].y
+			)
+		{
+			return true;
+		}
+		else 
+			return false;
+	}
+
+	return bRet;
+}
+
+bool CGestureDetect::IsInValidRange(NUI_SKELETON_DATA data, NUI_SKELETON_POSITION_INDEX indexLimitSmaller, NUI_SKELETON_POSITION_INDEX indexLimitBigger, 
+	NUI_SKELETON_POSITION_INDEX indexIn, bool bIsVertical)
+{
+	bool bRet = false;
+	if (bIsVertical)
+	{
+		if (data.SkeletonPositions[indexIn].y >= data.SkeletonPositions[indexLimitSmaller].y && 
+			data.SkeletonPositions[indexIn].y <= data.SkeletonPositions[indexLimitBigger].y)
+			bRet = true;
+	}
+	else
+	{
+		if (data.SkeletonPositions[indexIn].x >= data.SkeletonPositions[indexLimitSmaller].x && 
+			data.SkeletonPositions[indexIn].x <= data.SkeletonPositions[indexLimitBigger].x)
+			bRet = true;
+	}
+	return bRet;
 }
 
 void CGestureDetect::JudgeGesture( NUI_SKELETON_POSITION_INDEX posIndex )
 {
 	//计算当前手势
+#if 0
+	if (!IsInValidRange())
+	{
+		m_gestureType = GESTURE_NONE;
+		return ;
+	}
+	int nHorizonToRight = 0;
+	int nHorizonToLeft = 0;
+	int nVerticalToTop = 0;
+	int nVerticalToBottom = 0;
+	for (int i = 0; i < cSkeletonFrameMax - 1; i++)
+	{
+		if (m_dequeSkeletonData.at(i).SkeletonPositions[posIndex].x <= m_dequeSkeletonData.at(i+1).SkeletonPositions[posIndex].x)
+			nHorizonToRight++;
+		else
+			nHorizonToLeft++;
+		if (m_dequeSkeletonData.at(i).SkeletonPositions[posIndex].y <= m_dequeSkeletonData.at(i+1).SkeletonPositions[posIndex].y)
+			nVerticalToBottom++;
+		else
+			nVerticalToTop++;
+	}
+	m_gestureType = 0;
+	nHorizonToLeft > 10 ? m_gestureType |= GESTURE_TOLEFT : m_gestureType |= GESTURE_TORIGHT;
+	nVerticalToTop > 10 ? m_gestureType |= GESTURE_TOTOP : m_gestureType |= GESTURE_TOBOTTOM;
+#endif
+	m_gestureType = 0;
+	if (m_HandPointerInfo[0].X < m_HandPointerInfo[1].X)
+		m_gestureType |= GESTURE_TORIGHT;
+	else
+		m_gestureType |= GESTURE_TOLEFT;
+	if (m_HandPointerInfo[0].Y < m_HandPointerInfo[1].Y)
+		m_gestureType |= GESTURE_TOBOTTOM;
+	else
+		m_gestureType |= GESTURE_TOTOP;
+}
 
+void CGestureDetect::SetHandPointerInfo(NUI_HANDPOINTER_INFO data, int nIndex)
+{
+	switch(nIndex)
+	{
+	case 0:
+		m_HandPointerInfo[0] = data;
+		break;
+	case 1:
+		m_HandPointerInfo[1] = data;
+		break;
+	default:
+		break;
+	}
+}
+
+void CGestureDetect::GestureCallback(void(* GestureCalc)(unsigned int))
+{
+	GestureCalc(m_gestureType);
+}
+
+CString CGestureDetect::GetStringFromGestrue(unsigned int gesture)
+{
+	CString strRet;
+	if (gesture & GESTURE_NONE)
+	{
+		strRet.Append(L"None");
+	}
+	if (gesture & GESTURE_ACTIVE)
+	{
+		strRet.Append(L" 激活");
+	}
+	if (gesture & GESTURE_WAVE)
+	{
+		strRet.Append(L" 挥舞");
+	}
+	if (gesture & GESTURE_STAY)
+	{
+		strRet.Append(L" 原地");
+	}
+	if (gesture & GESTURE_TOLEFT)
+	{
+		strRet.Append(L" 向左");
+	}
+	if (gesture & GESTURE_TORIGHT)
+	{
+		strRet.Append(L" 向右");
+	}
+	if (gesture & GESTURE_TOTOP)
+	{
+		strRet.Append(L" 向上");
+	}
+	if (gesture & GESTURE_TOBOTTOM)
+	{
+		strRet.Append(L" 向下");
+	}
+	if (gesture & GESTURE_TOFRONT)
+	{
+		strRet.Append(L" 向前");
+	}
+	if (gesture & GESTURE_TOBACK)
+	{
+		strRet.Append(L" 向后");
+	}
+	return strRet;
 }
--- GestureDetect.h
+++ GestureDetect.h
@@ -1,23 +1,38 @@
 #pragma once
 
 #include "NuiApi.h"
+#include "KinectInteraction.h"
 #include <deque>
 
+#if 0
 enum GESTURE_TYPE
 {
-	GESTURE_NONE = 0,
-	GESTURE_ACTIVE,
-	GESTURE_WAVE,
-	GESTURE_STAY,
-	GESTURE_TOLEFT,
-	GESTURE_TORIGHT,
-	GESTURE_TOTOP,
-	GESTURE_TOBOTTOM,
-	GESTURE_TOFRONT,
-	GESTURE_TOBACK
+	GESTURE_NONE = 0x00000001,
+	GESTURE_ACTIVE = 0x00000002,
+	GESTURE_WAVE = 0x00000004,
+	GESTURE_STAY = 0x00000008,
+	GESTURE_TOLEFT = 0x00000010,
+	GESTURE_TORIGHT = 0x00000020,
+	GESTURE_TOTOP = 0x00000040,
+	GESTURE_TOBOTTOM = 0x00000080,
+	GESTURE_TOFRONT = 0x00000100,
+	GESTURE_TOBACK = 0x00000200
 };
+#endif
+
+#define GESTURE_NONE	 0x00000001
+#define GESTURE_ACTIVE	 0x00000002
+#define GESTURE_WAVE	 0x00000004
+#define GESTURE_STAY	 0x00000008
+#define GESTURE_TOLEFT	 0x00000010
+#define GESTURE_TORIGHT	 0x00000020
+#define GESTURE_TOTOP	 0x00000040
+#define GESTURE_TOBOTTOM 0x00000080
+#define GESTURE_TOFRONT  0x00000100
+#define GESTURE_TOBACK	 0x00000200
 
 const int cSkeletonFrameMax = 20;	//存储20个骨骼帧数据，据此算出当前手势
+const int cGestrueLimit = cSkeletonFrameMax / 2;
 
 class CGestureDetect
 {
@@ -31,14 +46,23 @@ public:
 	//设置骨骼信息
 	void SetSkeletonFrame(const NUI_SKELETON_FRAME& SkeletonFrame);
 	//获取当前手势
-	void GetCurrentGesture(GESTURE_TYPE& GestureType, bool isLeftHand = true);
+	unsigned int GetCurrentGesture();
 
-private:
+	bool IsInValidRange();
+	//判断数据
+	bool IsInValidRange(NUI_SKELETON_DATA data, NUI_SKELETON_POSITION_INDEX indexLimitSmaller, NUI_SKELETON_POSITION_INDEX indexLimitBigger, 
+						NUI_SKELETON_POSITION_INDEX indexIn, bool bIsVertical = false);
+
+	void GestureCallback(void(* GestureCalc)(unsigned int));
+	CString GetStringFromGestrue(unsigned int gesture);
+
 	void JudgeGesture(NUI_SKELETON_POSITION_INDEX posIndex = NUI_SKELETON_POSITION_HAND_LEFT);
-
+	
+	void SetHandPointerInfo(NUI_HANDPOINTER_INFO data, int nIndex = 0);
 private:
-	std::deque<NUI_SKELETON_FRAME>	m_dequeSkeletonFrame;
-	GESTURE_TYPE					m_gestureType;
+	std::deque<NUI_SKELETON_DATA>	m_dequeSkeletonData;
+	unsigned int					m_gestureType;
 	NUI_SKELETON_POSITION_INDEX		m_skeletonPosIndex;
+	NUI_HANDPOINTER_INFO			m_HandPointerInfo[2];
 };
 
